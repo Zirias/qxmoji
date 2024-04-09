2a4d5c7bb9602b61af67d0b2ba252de5826e4933
Felix Palmen <felix@palmen-it.de>
2024-04-09 19:27:39 +0200

    SettingsDlg: Add tooltips

--- src/bin/qxmoji/settingsdlg.cpp
+++ src/bin/qxmoji/settingsdlg.cpp
@@ -25,11 +25,24 @@ SettingsDlgPrivate::SettingsDlgPrivate(SettingsDlg *dlg) :
     scaleLabel("Scale size:"),
     waitLabel("Keymap reset wait (ms):")
 {
+    scaleLabel.setToolTip("Scale up the size of displayed Emojis.\n"
+	    "Tiny means no scaling (same size as default Window font)");
+    scaleSelect.setToolTip(scaleLabel.toolTip());
     QMetaEnum scaleEnum = QMetaEnum::fromType<EmojiFont::Scale>();
     for (int i = 0; i < scaleEnum.keyCount(); ++i)
     {
 	scaleSelect.addItem(scaleEnum.key(i), scaleEnum.value(i));
     }
+    waitLabel.setToolTip("qXmoji sends emoji codepoints as key press events.\n"
+	    "Therefore, the keyboard mapping must be temporarily changed.\n"
+	    "This is the time in ms to wait before restoring the mapping\n"
+	    "after sending the keyboard events.\n"
+	    "\n"
+	    "Raising this might help if emojis don't \"arrive\" at all or you\n"
+	    "see basic (ASCII) characters instead.\n"
+	    "It won't help when a single emoji is shown as two or more\n"
+	    "symbols, this is handled by the receiving client.");
+    waitSelect.setToolTip(waitLabel.toolTip());
     waitSelect.setMinimum(0);
     waitSelect.setMaximum(500);
     waitSelect.setSingleStep(10);
