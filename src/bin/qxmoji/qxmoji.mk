dfc1f10855d4433ba66b4ea0ba69f8a6dee81769
Felix Palmen <felix@palmen-it.de>
2024-04-10 09:45:13 +0200

    Update zimk, use improved Qt support

--- src/bin/qxmoji/qxmoji.mk
+++ src/bin/qxmoji/qxmoji.mk
@@ -1,18 +1,17 @@
 qxmoji_VERSION=		0.1
+qxmoji_USES=		qt
+qxmoji_USE_QT=		Core Gui Widgets
+qxmoji_USE_QT5=		X11Extras
 qxmoji_MODULES=		emoji \
 			xkeyinjector
 qxmoji_CXXMODULES=	flowlayout \
-			main \
-			$(qxmoji_PREPROCMODULES)
-qxmoji_PREPROCMODULES=	emojibutton \
+			main
+qxmoji_MOCMODULES=	emojibutton \
 			emojifont \
 			settingsdlg \
 			qxmoji \
 			qxmojiwin
-qxmoji_PREPROC=		MOC
-qxmoji_PREPROCFLAGS=	-p.
 qxmoji_QRC=		icon
-qxmoji_INCLUDES=	-I$(qxmoji_PPSRCDIR)
 qxmoji_CXXFLAGS=	-fPIC
 qxmoji_PKGDEPS=		xcb \
 			xcb-xtest
@@ -24,17 +23,6 @@ qxmoji_E2C_FILES=	emoji.c:contrib/emoji-test.txt
 qxmoji_ICONSIZES=	16x16 32x32 48x48 256x256
 qxmoji_DESKTOPFILE=	qxmoji
 qxmoji_DOCS=		README.md
-
-ifeq ($(QT_VERSION),5)
-qxmoji_PKGDEPS+=	Qt5Core \
-			Qt5Gui \
-			Qt5Widgets \
-			Qt5X11Extras
-else
-qxmoji_PKGDEPS+=	Qt6Core \
-			Qt6Gui \
-			Qt6Widgets
-endif
 
 GEN_E2C_tool=		$(HOSTSH) scripts/emoji2cdef.sh
 
