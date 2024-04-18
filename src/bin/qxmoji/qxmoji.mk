73f556073635599b853d62c0faccf095995195df
Felix Palmen <felix@palmen-it.de>
2024-04-18 10:54:23 +0200

    qxmoji.mk: *_USE_QT=Core is implicit, remove

--- src/bin/qxmoji/qxmoji.mk
+++ src/bin/qxmoji/qxmoji.mk
@@ -1,6 +1,6 @@
 qxmoji_VERSION=		0.5
 qxmoji_USES=		qt
-qxmoji_USE_QT=		Core Gui Network Widgets
+qxmoji_USE_QT=		Gui Network Widgets
 qxmoji_USE_QT5=		X11Extras
 qxmoji_MODULES=		emojisearch \
 			main \
